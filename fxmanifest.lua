958f1960b858ed6d6de0275649a52080085c1e27
Peter <20475516+SirOMGitsYOU@users.noreply.github.com>
2025-08-13 12:59:59 +0100

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 "yes"
+
+author 'poitor'
+description 'US Flag replacement to British Flags for FiveM'
+version '1.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.yft',
+    'stream/*.ydr',
+    'stream/*.ytd',
+}
